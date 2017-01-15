494fa7daff04cbe1acc5bf204a653aadabd222fb
zhangxinyu <583145128@qq.com>
2017-01-15 10:15:43 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-
+use Auth;
 class UsersController extends Controller
 {
     public function create(){
@@ -29,6 +29,7 @@ class UsersController extends Controller
             'email'=>$request->email,
             'password'=>$request->password,
         ]);
+        Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
     }
