a45297017aa1cdf3ce16cbf8ce3b931b44540cfb
zhangxinyu <583145128@qq.com>
2017-01-15 04:28:28 +0000

    Update database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$db_config = get_db_config();
 return [
 
     /*
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],//env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,10 +66,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $db_config['host'],//env('DB_HOST', 'localhost'),
+            'database' => $db_config['database'],//env('DB_DATABASE', 'forge'),
+            'username' => $db_config['username'],//env('DB_USERNAME', 'forge'),
+            'password' => $db_config['password'],//env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
